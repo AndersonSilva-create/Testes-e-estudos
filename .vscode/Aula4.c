041e3ee71b8fb6f1f8c427353d14b9b33c050fdf
AndersonSilva-create <nunesanderson995@gmail.com>
2026-05-08 22:22:59 +0000

    aula 4

--- .vscode/Aula4.c
+++ .vscode/Aula4.c
@@ -1,7 +1,25 @@
-#include <stdio.h>>
+#include <stdio.h>
 
 int main() {
-    int contador = 1;
+    int b = 1, r = 1;
+    printf("Torre:\n");
+
+    for (int i = 1; i <= 5; i++)
+    {
+        printf("Direita\n");
+    }
+    printf("Bispo:\n");
+    while (b <= 5)
+    {
+        printf("Direita\nEsquerda\n");
+        b++;
+    }
+    printf("Rainha:\n");
+    do {
+        printf("Direita\n");
+        r++;
+    } while (r <= 8);   
+
     
     
     return 0;
